ad6768b9e536b09707d9722d411de911545edd9e
niteshsingh <er.niteshkrsingh@gmail.com>
2025-12-13 08:29:46 +0530

    Initial commit

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const app = require("./src/app");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
